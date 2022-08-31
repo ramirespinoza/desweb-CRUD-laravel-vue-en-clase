d1db3ad546ad3bae1d16ae877fe5d7af86e5c0cf
Carlos Ramirez <ramirespinoza@gmail.com>
2022-08-31 14:37:54 -0600

    Se crea el Model y el Controller para la tabla customer, se indica la ruta para este Modelo

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends('app')
+
+
+@section('content')
+
+on dashbord
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
+
+
+Route::resource('customer', 'CustomerController', ['except' => 'show']);
